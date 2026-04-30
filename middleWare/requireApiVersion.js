e0a81da94bec764e3b8d43d766d86ab5eb40c6d6
chika-12 <markworship001@gmail.com>
2026-04-30 21:56:45 +0100

    feature(scope):fixed versionin

--- middleWare/requireApiVersion.js
+++ middleWare/requireApiVersion.js
@@ -1,4 +1,15 @@
 module.exports = (req, res, next) => {
+  const exempt = [
+    '/auth/github',
+    '/auth/github/callback',
+    '/auth/seed-analyst',
+    '/auth/seed-admin',
+    '/auth/refresh',
+    '/auth/logout',
+  ];
+
+  if (exempt.some((path) => req.path.includes(path))) return next();
+
   const version = req.headers['api-version'] || req.headers['x-api-version'];
   if (!version) {
     return res.status(400).json({
