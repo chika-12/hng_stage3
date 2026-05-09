17e8cd6eb39af4f8681b84fa8ba889422973fb32
chika-12 <markworship001@gmail.com>
2026-05-09 13:31:55 +0100

    feature: adding file path for csv import

--- middleWare/upload.js
+++ middleWare/upload.js
@@ -1,19 +1,26 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 const AppError = require('../utils/appError');
 
+const uploadDir = 'tmp/uploads';
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, 'tmp/uploads/'),
+  destination: (req, file, cb) => cb(null, uploadDir),
   filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`),
 });
 
 const upload = multer({
   storage,
   fileFilter: (req, file, cb) => {
-    if (path.extname(file.originalname) != '.csv') {
+    if (path.extname(file.originalname) !== '.csv') {
       return cb(new AppError('Only CSV files allowed', 403));
     }
     cb(null, true);
   },
 });
+
 module.exports = upload;
